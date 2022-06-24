b57c45633bd7fbf04ed9137e6e46a0c46d1db00f
Martin Beran <martin@mber.cz>
2022-06-24 23:05:35 +0200

    Created empty files for user documentation, parser, and parser tests

---
+++ src/threadscript/parser.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+/*! \file
+ * \brief A generic recursive descent parser
+ *
+ * \test in file test_parser.cpp
+ */
+
+//! The namespace containing a generic recursive descent parser
+/*! This is not a ThreadScript parser, it is a framework for creating parsers.
+ * \test in file test_parser.cpp */
+namespace threadscript::parser {
+
+} // namespace threadscript::parser
---
+++ test/test_parser.cpp
@@ -0,0 +1,16 @@
+/*! \file
+ * \brief Tests of namespace threadscript::parser
+ */
+
+//! \cond
+#include "threadscript/parser.hpp"
+
+#define BOOST_TEST_MODULE parser
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(dummy)
+{
+    BOOST_TEST(true);
+}
+//! \endcond
