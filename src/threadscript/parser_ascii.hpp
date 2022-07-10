773a7244999db0cd4e7708352b6e8536bbfa669c
Martin Beran <martin@mber.cz>
2022-07-10 19:29:23 +0200

    Class parser_ascii::rules::factory and tests of it

--- src/threadscript/parser_ascii.hpp
+++ src/threadscript/parser_ascii.hpp
@@ -56,7 +56,7 @@ using handler = parser::default_handler<Ctx, Self, Up, Info, It>;
  * current locale.
  * \param[in] c a character
  * \return an uppercase \a c converted to lowercase, otherwise \a c unchanged */
-[[nodiscard]] inline char to_lower(char c)
+[[nodiscard]] inline char to_lower(char c) noexcept
 {
     return char(c >= 'A' && c <= 'Z' ? c - 'A' + 'a' : c);
 }
@@ -66,7 +66,7 @@ using handler = parser::default_handler<Ctx, Self, Up, Info, It>;
  * current locale.
  * \param[in] c a character
  * \return an uppercase \a c converted to lowercase, otherwise \a c unchanged */
-[[nodiscard]] inline char to_upper(char c)
+[[nodiscard]] inline char to_upper(char c) noexcept
 {
     return char(c >= 'a' && c <= 'z' ? c - 'a' + 'A' : c);
 }
@@ -80,7 +80,7 @@ public:
     /*! \param[in] a the first character
      * \param[in] b the second character
      * \return if \a a and \a b are equal */
-    bool operator()(char a, char b) {
+    bool operator()(char a, char b) const noexcept {
         return to_lower(a) == to_lower(b);
     }
 };
@@ -91,147 +91,108 @@ public:
  * \test in file test_parser_ascii.cpp */
 namespace rules {
 
-//! Creates a rule that always fails.
-/*! \tparam Ctx a parsing context
- * \tparam Self a temporary context of this rule
- * \tparam Up a temporary context of a parent rule
+//! Creates rules with a common set of template parameters.
+/*! This template provides an easy way to specify set of template parameters
+ * that cannot be deduced from arguments of rule creation functions. An alias
+ * can be defined for an instance of this template and then used for creating
+ * individual rules. See test_parser_ascii.cpp for examples how to use this
+ * class.
  * \tparam It an iterator to the input sequence
- * \return the created rule */
-template <class Ctx, class Self, class Up, ascii_iterator It>
-parser::rules::fail<Ctx, Self, Up, It,
-    handler<Ctx, Self, Up, parser::empty, It>>
-fail();
-
-//! Creates a rule that matches the end of input.
-/*! This rule does not consume any input, therefore it must not be used in an
- * unlimited rules::repeat, because it would create an endless loop.
  * \tparam Ctx a parsing context
  * \tparam Self a temporary context of this rule
- * \tparam Up a temporary context of a parent rule
- * \tparam It an iterator to the input sequence of terminal symbols
- * \return the created rule */
-template <class Ctx, class Self, class Up, ascii_iterator It>
-parser::rules::eof<Ctx, Self, Up, It, handler<Ctx, Self, Up, parser::empty, It>>
-eof();
-
-//! Creates a rule that matches any single terminal symbol.
-/*! \tparam Ctx a parsing context
- * \tparam Self a temporary context of this rule
- * \tparam Up a temporary context of a parent rule
- * \tparam It an iterator to the input sequence of terminal symbols
- * \return the created rule */
-template <class Ctx, class Self, class Up, ascii_iterator It>
-parser::rules::any<Ctx, Self, Up, It, handler<Ctx, Self, Up, parser::empty, It>>
-any();
-
-//! Creates a rule that matches a specific single \c char.
-/*! \tparam Ctx a parsing context
- * \tparam Self a temporary context of this rule
- * \tparam Up a temporary context of a parent rule
- * \tparam It an iterator to the input sequence of terminal symbols
- * \param[in] c the matching character
- * \return the created rule */
-template <class Ctx, class Self, class Up, ascii_iterator It>
-parser::rules::t<Ctx, Self, Up, It, handler<Ctx, Self, Up, parser::empty, It>>
-t(char c)
-{
-    return {c};
-}
-
-//! Creates a rule that matches a \c char for which a predicate returns \c true
-/*! \tparam Ctx a parsing context
- * \tparam Self a temporary context of this rule
- * \tparam Up a temporary context of a parent rule
- * \tparam It an iterator to the input sequence of terminal symbols
- * \tparam Predicate a predicate for testing if a symbol matches
- * \param[in] pred the predicate for testing input
- * \return the created rule */
-template <class Ctx, class Self, class Up, ascii_iterator It,
-    predicate<It> Predicate>
-parser::rules::p<Ctx, Self, Up, It, Predicate,
-    handler<Ctx, Self, Up, parser::empty, It>>
-p(Predicate pred)
-{
-    return {std::move(pred)};
-}
-
-//! Creates a rule that matches a \c std::string.
-/*! It tests that a predicate returns \c true for corresponding \c char from
- * the input and from the stored \c std::string.
- * \tparam Ctx a parsing context
- * \tparam Self a temporary context of this rule
- * \tparam Up a temporary context of a parent rule
- * \tparam It an iterator to the input sequence of \c char
- * \tparam Predicate a predicate for testing if a \c char matches; by default,
- * an equality test is performed, a common alternative is case-insensitive
- * matching by equal_ic.
- * \param[in] seq a string to be matched; stored in the created rule object
- * \param[in] pred the predicate for testing characters
- * \return the created rule */
-template <class Ctx, class Self, class Up, ascii_iterator It,
-    predicate2<It> Predicate = std::equal_to<char>>
-parser::rules::str<Ctx, Self, Up, It, std::string, Predicate,
-    handler<Ctx, Self, Up, parser::empty, It>>
-str(std::string seq, Predicate pred = {})
-{
-    return {std::move(seq), std::move(pred)};
-}
-
-//! Creates a rule for case-insensitive matching of a \c std::string.
-/*! \tparam Ctx a parsing context
- * \tparam Self a temporary context of this rule
- * \tparam Up a temporary context of a parent rule
- * \tparam It an iterator to the input sequence of \c char
- * \param[in] seq a string to be matched; stored in the created rule object
- * \return the created rule */
-template <class Ctx, class Self, class Up, ascii_iterator It>
-parser::rules::str<Ctx, Self, Up, It, std::string, equal_ic,
-    handler<Ctx, Self, Up, parser::empty, It>>
-str_ic(std::string seq)
-{
-    return {std::move(seq), equal_ic()};
-}
-
-//! Creates a rule that matches a \c std::string_view.
-/*! It tests that a predicate returns \c true for corresponding \c char from
- * the input and from the stored \c std::string_view.
- * \tparam Ctx a parsing context
- * \tparam Self a temporary context of this rule
- * \tparam Up a temporary context of a parent rule
- * \tparam It an iterator to the input sequence of \c char
- * \tparam Predicate a predicate for testing if a \c char matches; by default,
- * an equality test is performed, a common alternative is case-insensitive
- * matching by equal_ic.
- * \param[in] seq a string view to be matched; the created rule object contains
- * only a reference to an externally stored sequences of characters, which must
- * not be destroyed during the lifetime of the created rule
- * \param[in] pred the predicate for testing characters
- * \return the created rule */
-template <class Ctx, class Self, class Up, ascii_iterator It,
-    predicate2<It> Predicate = std::equal_to<char>>
-parser::rules::str<Ctx, Self, Up, It, std::string_view, Predicate,
-    handler<Ctx, Self, Up, parser::empty, It>>
-str(std::string_view seq, Predicate pred = {})
-{
-    return {seq, std::move(pred)};
-}
-
-//! Creates a rule for case-insensitive matching of a \c std::string_view.
-/*! \tparam Ctx a parsing context
- * \tparam Self a temporary context of this rule
- * \tparam Up a temporary context of a parent rule
- * \tparam It an iterator to the input sequence of \c char
- * \param[in] seq a string view to be matched; the created rule object contains
- * only a reference to an externally stored sequences of characters, which must
- * not be destroyed during the lifetime of the created rule
- * \return the created rule */
-template <class Ctx, class Self, class Up, ascii_iterator It>
-parser::rules::str<Ctx, Self, Up, It, std::string_view, equal_ic,
-    handler<Ctx, Self, Up, parser::empty, It>>
-str_ic(std::string_view seq)
-{
-    return {seq, equal_ic()};
-}
+ * \tparam Up a temporary context of a parent rule */
+template <ascii_iterator It, class Ctx = parser::context,
+    class Self = parser::empty, class Up = parser::empty>
+class factory {
+public:
+    //! Creates parser::rules::fail
+    /*! \return the created rule */
+    static auto fail() {
+        return parser::rules::fail<Ctx, Self, Up, It,
+            handler<Ctx, Self, Up, parser::empty, It>>{};
+    }
+    //! Creates parser::rules::eof
+    /*! This rule does not consume any input, therefore it must not be used in
+     * an unlimited rules::repeat, because it would create an endless loop.
+     * \return the created rule */
+    static auto eof() {
+        return parser::rules::eof<Ctx, Self, Up, It,
+            handler<Ctx, Self, Up, parser::empty, It>>{};
+    }
+    //! Creates parser::rules::any
+    /*! \return the created rule */
+    static auto any() {
+        return parser::rules::any<Ctx, Self, Up, It,
+            handler<Ctx, Self, Up, parser::empty, It>>{};
+    }
+    //! Creates parser::rules::t
+    /*! \param[in] c the matching character
+     * \return the created rule */
+    static auto t(char c) {
+        return parser::rules::t<Ctx, Self, Up, It,
+            handler<Ctx, Self, Up, parser::empty, It>>{c};
+    }
+    //! Creates parser::rules::p
+    /*! \tparam Predicate a predicate for testing if a symbol matches
+     * \param[in] pred the predicate for testing input
+     * \return the created rule */
+    template <predicate<It> Predicate>
+    static auto p(Predicate pred) {
+        return parser::rules::p<Ctx, Self, Up, It, Predicate,
+            handler<Ctx, Self, Up, parser::empty, It>>{std::move(pred)};
+    }
+    //! Creates parser::rules::str
+    /*! It tests that a predicate returns \c true for corresponding \c char
+     * from the input and from the stored \c std::string.
+     * \tparam Predicate a predicate for testing if a \c char matches; by
+     * default, an equality test is performed, a common alternative is
+     * case-insensitive matching by equal_ic.
+     * \param[in] seq a string to be matched; stored in the created rule object
+     * \param[in] pred the predicate for testing characters
+     * \return the created rule */
+    template <predicate2<It> Predicate = std::equal_to<char>>
+    static auto str(std::string seq, Predicate pred = {}) {
+        return parser::rules::str<Ctx, Self, Up, It, std::string, Predicate,
+            handler<Ctx, Self, Up, parser::empty, It>>{std::move(seq),
+                std::move(pred)};
+    }
+    //! Creates parser::rules::str
+    /*! \param[in] seq a string to be matched; stored in the created rule object
+     * \return the created rule */
+    static auto str_ic(std::string seq) {
+        return parser::rules::str<Ctx, Self, Up, It, std::string, equal_ic,
+            handler<Ctx, Self, Up, parser::empty, It>>{std::move(seq),
+                equal_ic()};
+    }
+    //! Creates parser::rules::str
+    /*! It tests that a predicate returns \c true for corresponding \c char from
+     * the input and from the stored \c std::string_view.
+     * \tparam Predicate a predicate for testing if a \c char matches; by
+     * default, an equality test is performed, a common alternative is
+     * case-insensitive matching by equal_ic.
+     * \param[in] seq a string view to be matched; the created rule object
+     * contains only a reference to an externally stored sequences of
+     * characters, which must not be destroyed during the lifetime of the
+     * created rule
+     * \param[in] pred the predicate for testing characters
+     * \return the created rule */
+    template <predicate2<It> Predicate = std::equal_to<char>>
+    static auto str(std::string_view seq, Predicate pred = {}) {
+        return parser::rules::str<Ctx, Self, Up, It,
+            std::string_view, Predicate,
+            handler<Ctx, Self, Up, parser::empty, It>>{seq, std::move(pred)};
+    }
+    //! Creates parser::rules::str
+    /*! \param[in] seq a string view to be matched; the created rule object
+     * contains only a reference to an externally stored sequences of
+     * characters, which must not be destroyed during the lifetime of the
+     * created rule
+     * \return the created rule */
+    static auto str_ic(std::string_view seq) {
+        return parser::rules::str<Ctx, Self, Up, It, std::string_view, equal_ic,
+            handler<Ctx, Self, Up, parser::empty, It>>{seq, equal_ic()};
+    }
+};
 
 } // namespace rules
 
--- test/test_parser_ascii.cpp
+++ test/test_parser_ascii.cpp
@@ -3,8 +3,11 @@
  */
 
 //! \cond
+#include "threadscript/parser.hpp"
 #include "threadscript/parser_ascii.hpp"
 #include <cctype>
+#include <limits>
+#include <type_traits>
 
 #define BOOST_TEST_MODULE parser
 #define BOOST_TEST_DYN_LINK
@@ -54,14 +57,258 @@ std::ostream& operator<<(std::ostream& os, const parsed& v)
     return os;
 }
 
+void test_parse(auto&& sample, auto&& rule)
+{
+    auto it = tsp::make_script_iterator(sample.text);
+    tsp::context ctx;
+    using it_t = typename decltype(it)::first_type;
+    BOOST_TEST_MESSAGE("result=" << sample.result);
+    if (sample.result)
+        BOOST_REQUIRE_NO_THROW(
+            try {
+                ctx.parse(rule, it);
+            } catch (std::exception& e) {
+                BOOST_TEST_INFO("exception: " << e.what());
+                throw;
+            });
+    else
+        BOOST_CHECK_EXCEPTION(ctx.parse(rule, it),
+            tsp::error<it_t>,
+            ([it, &sample](auto&& e) {
+                BOOST_CHECK_EQUAL(e.what(), sample.error);
+                BOOST_CHECK_EQUAL(e.pos().line, sample.line);
+                BOOST_CHECK_EQUAL(e.pos().column, sample.column);
+                return true;
+            }));
+}
+
 } // namespace test
 //! \endcond
 
 /* \file
- * \test \c dummy -- a dummy test (to be removed) of
- * threadscript::parser_ascii::rules::... */
+ * \test \c to_lower -- test of threadscript::parser_ascii::to_lower() */
 //! \cond
-BOOST_AUTO_TEST_CASE(dummy)
+BOOST_AUTO_TEST_CASE(to_lower)
 {
+    std::array<char, 256> chars{};
+    for (int i = 0; i < 256; ++i)
+        chars[i] = char(i);
+    char c = 'a';
+    for (int i = 'A'; i <= 'Z'; ++i)
+        chars[i] = char(c++);
+    for (int i = 0; i < 256; ++i)
+        BOOST_CHECK_EQUAL(tspa::to_lower(char(i)), chars[i]);
+}
+//! \endcond
+
+/* \file
+ * \test \c to_upper -- test of threadscript::parser_ascii::to_upper() */
+//! \cond
+BOOST_AUTO_TEST_CASE(to_upper)
+{
+    std::array<char, 256> chars{};
+    for (int i = 0; i < 256; ++i)
+        chars[i] = char(i);
+    char c = 'A';
+    for (int i = 'a'; i <= 'z'; ++i)
+        chars[i] = char(c++);
+    for (int i = 0; i < 256; ++i)
+        BOOST_CHECK_EQUAL(tspa::to_upper(char(i)), chars[i]);
+}
+//! \endcond
+
+/*! \file
+ * \test \c equal_ic -- test of threadscript::parser_ascii::equal_ic */
+//! \cond
+BOOST_AUTO_TEST_CASE(equal_ic)
+{
+    for (int a = 0; a < 256; ++a)
+        for (int b = 0; b < 256; ++b) {
+            BOOST_TEST_INFO("a=" << a << " b=" << b);
+            bool res = a == b;
+            if (a >= 'a' && a <= 'z')
+                res = res || tspa::to_upper(char(a)) == b;
+            if (b >= 'a' && b <= 'z')
+                res = res || a == tspa::to_upper(char(b));
+            BOOST_CHECK_EQUAL(tspa::equal_ic()(a, b), res);
+        }
+}
+//! \endcond
+
+/*! \file
+ * \test \c fail -- test of threadscript::parser_ascii::rules::factory::fail */
+//! \cond
+BOOST_DATA_TEST_CASE(fail, (std::vector<test::parsed>{
+                                {"", false, 1, 1},
+                                {"nonempty", false, 1, 1},
+                     }))
+{
+    using f = tsra::factory<tsp::script_iterator<
+        typename decltype(sample.text)::const_iterator>>;
+    auto rule = f::fail();
+    test_parse(sample, rule);
+}
+//! \endcond
+
+/*! \file
+ * \test \c eof -- test of threadscript::parser_ascii::rules::factory::eof */
+//! \cond
+BOOST_DATA_TEST_CASE(eof, (std::vector<test::parsed>{
+                               {"", true, 1, 1},
+                               {"nonempty", false, 1, 1},
+                     }))
+{
+    using f = tsra::factory<tsp::script_iterator<
+        typename decltype(sample.text)::const_iterator>>;
+    auto rule = f::eof();
+    test_parse(sample, rule);
+}
+//! \endcond
+
+/*! \file
+ * \test \c any -- test of threadscript::parser_ascii::rules::factory::any */
+//! \cond
+BOOST_DATA_TEST_CASE(any, (std::vector<test::parsed>{
+                               {"", false, 1, 1},
+                               {"A", true, 1, 2},
+                               {"nonempty", false, 1, 2, "Partial match"},
+                               {"\n", true, 2, 1},
+                     }))
+{
+    using f = tsra::factory<tsp::script_iterator<
+        typename decltype(sample.text)::const_iterator>>;
+    auto rule = f::any();
+    test_parse(sample, rule);
+}
+//! \endcond
+
+/*! \file
+ * \test \c t -- test of threadscript::parser_ascii::rules::factory::t */
+//! \cond
+BOOST_DATA_TEST_CASE(t, (std::vector<test::parsed>{
+                             {"", false, 1, 1},
+                             {"A", true, 1, 2},
+                             {"a", false, 1, 1},
+                             {"?", false, 1, 1},
+                             {"A nonempty", false, 1, 2, "Partial match"},
+                     }))
+{
+    using f = tsra::factory<tsp::script_iterator<
+        typename decltype(sample.text)::const_iterator>>;
+    auto rule = f::t('A');
+    test_parse(sample, rule);
+}
+//! \endcond
+
+/*! \file
+ * \test \c p -- test of threadscript::parser_ascii::rules::factory::p */
+//! \cond
+BOOST_DATA_TEST_CASE(p, (std::vector<test::parsed>{
+                             {"", false, 1, 1},
+                             {"A", true, 1, 2},
+                             {"B", true, 1, 2},
+                             {"a", false, 1, 1},
+                             {"?", false, 1, 1},
+                             {"A nonempty", false, 1, 2, "Partial match"},
+                             {"Z nonempty", false, 1, 2, "Partial match"},
+                     }))
+{
+    using f = tsra::factory<tsp::script_iterator<
+        typename decltype(sample.text)::const_iterator>>;
+    auto rule = f::p(
+        [](const char& c) {
+            return c >= 'A' && c <= 'Z';
+        });
+    test_parse(sample, rule);
+}
+//! \endcond
+
+/*! \file
+ * \test \c str_string -- test of
+ * threadscript::parser_ascii::rules::factory::str matching \c std::string */
+//! \cond
+BOOST_DATA_TEST_CASE(str_string, (std::vector<test::parsed>{
+                                {"", false, 1, 1},
+                                {"A", false, 1, 2},
+                                {"AB", false, 1, 3},
+                                {"ABC", true, 1, 4},
+                                {"aBC", false, 1, 1},
+                                {"AbC", false, 1, 2},
+                                {"ABc", false, 1, 3},
+                                {"ABC nonempty", false, 1, 4, "Partial match"},
+                                }))
+{
+    using f = tsra::factory<tsp::script_iterator<
+        typename decltype(sample.text)::const_iterator>>;
+    auto rule = f::str("ABC"s);
+    test_parse(sample, rule);
+}
+//! \endcond
+
+/*! \file
+ * \test \c str_ic_string -- test of
+ * threadscript::parser_ascii::rules::factory::str_ic matching \c std::string */
+//! \cond
+BOOST_DATA_TEST_CASE(str_ic_string, (std::vector<test::parsed>{
+                                {"", false, 1, 1},
+                                {"A", false, 1, 2},
+                                {"AB", false, 1, 3},
+                                {"ABC", true, 1, 4},
+                                {"aBC", true, 1, 4},
+                                {"AbC", true, 1, 4},
+                                {"ABc", true, 1, 4},
+                                {"ABC nonempty", false, 1, 4, "Partial match"},
+                                }))
+{
+    using f = tsra::factory<tsp::script_iterator<
+        typename decltype(sample.text)::const_iterator>>;
+    auto rule = f::str_ic("ABC"s);
+    test_parse(sample, rule);
+}
+//! \endcond
+
+/*! \file
+ * \test \c str_string_view -- test of
+ * threadscript::parser_ascii::rules::factory::str matching
+ * \c std::string_view */
+//! \cond
+BOOST_DATA_TEST_CASE(str_string_view, (std::vector<test::parsed>{
+                                {"", false, 1, 1},
+                                {"A", false, 1, 2},
+                                {"AB", false, 1, 3},
+                                {"ABC", true, 1, 4},
+                                {"aBC", false, 1, 1},
+                                {"AbC", false, 1, 2},
+                                {"ABc", false, 1, 3},
+                                {"ABC nonempty", false, 1, 4, "Partial match"},
+                                }))
+{
+    using f = tsra::factory<tsp::script_iterator<
+        typename decltype(sample.text)::const_iterator>>;
+    auto rule = f::str("ABC"sv);
+    test_parse(sample, rule);
+}
+//! \endcond
+
+/*! \file
+ * \test \c str_ic_string_view -- test of
+ * threadscript::parser_ascii::rules::factory::str_ic matching
+ * \c std::string_view */
+//! \cond
+BOOST_DATA_TEST_CASE(str_ic_string_view, (std::vector<test::parsed>{
+                                {"", false, 1, 1},
+                                {"A", false, 1, 2},
+                                {"AB", false, 1, 3},
+                                {"ABC", true, 1, 4},
+                                {"aBC", true, 1, 4},
+                                {"AbC", true, 1, 4},
+                                {"ABc", true, 1, 4},
+                                {"ABC nonempty", false, 1, 4, "Partial match"},
+                                }))
+{
+    using f = tsra::factory<tsp::script_iterator<
+        typename decltype(sample.text)::const_iterator>>;
+    auto rule = f::str_ic("ABC"sv);
+    test_parse(sample, rule);
 }
 //! \endcond
