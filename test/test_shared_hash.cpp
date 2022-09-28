21344a3e8a94c039c5d509fc26fb2e82694c414e
Martin Beran <martin@mber.cz>
2022-09-28 18:34:18 +0200

    WiP: Tests of class shared_hash

--- test/test_shared_hash.cpp
+++ test/test_shared_hash.cpp
@@ -4,21 +4,28 @@
 
 //! \cond
 #include "threadscript/threadscript.hpp"
+#include "threadscript/symbol_table_impl.hpp"
 
 #define BOOST_TEST_MODULE shared_hash
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
 
-namespace ts = threadscript;
-using namespace std::string_literals;
-using namespace std::string_view_literals;
+#include "script_runner.hpp"
+
+#include <thread>
+
+auto sh_vars = test::make_sh_vars<ts::shared_hash>();
 //! \endcond
 
 /*! \file
- * \test \c dummy -- A dummy test, to be replaced by real tests */
+ * \test \c create_object -- Creates a threadscript::basic_shared_hash object */
 //! \cond
-BOOST_AUTO_TEST_CASE(dummy)
+BOOST_DATA_TEST_CASE(create_object, (std::vector<test::runner_result>{
+    {R"(type(shared_hash()))", "shared_hash", ""},
+}))
 {
+    test::check_runner(sample, sh_vars);
 }
 //! \endcond
 
